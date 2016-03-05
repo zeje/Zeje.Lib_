8c7a474955e050ac6ec40bc98c957e09175afba0
zeje <zezhi821@163.com>
2016-03-05 13:43:16 +0800

    Zeje的个人类库

---
+++ Zeje.Core/PageJson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Zeje.Core
+{
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PageJson<T>
+    {
+        /// <summary>
+        /// </summary>
+        private PageJson()
+        { }
+        /// <summary>
+        /// </summary>
+        /// <param name="total"></param>
+        /// <param name="rows"></param>
+        public PageJson(int total, List<T> rows)
+        {
+            this.total = total;
+            this.rows = rows;
+        }
+        /// <summary>
+        /// </summary>
+        public int? total { get; set; }
+        /// <summary>
+        /// </summary>
+        public IEnumerable<T> rows { get; set; }
+    }
+}
